861b41fee3177dca35111bcaf372a37a1c3dd302
Sebas <sheva3232@gmail.com>
2023-06-21 17:22:35 -0500

    re ordering folders

--- polybase_integration/scripts/Tag/create.js
+++
@@ -1,9 +0,0 @@
-import { Polybase } from "@polybase/client";
-import 'dotenv/config';
-
-const db = new Polybase({
-  defaultNamespace: process.env.NAMESPACE,
-});
-
-await db.collection("Tag").create(["nft", "NFT"]);
-
