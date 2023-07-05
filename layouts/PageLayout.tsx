d26a43fe141e9df0cf916e376ce4f959bc85aacb
gitguti <gitmelgtrrz1@gmail.com>
2023-07-04 21:55:46 -0400

    [frontend] updated favicon

--- layouts/PageLayout.tsx
+++ layouts/PageLayout.tsx
@@ -16,11 +16,15 @@ type Props = {
   footer: boolean;
 };
 const PageLayout = ({ title, children, footer }: Props) => {
-  const pageTitle = `DAOTool | ${title}`;
+  const pageTitle = `Join | ${title}`;
   return (
     <>
       <Head>
         <title>{pageTitle}</title>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+<link rel="manifest" href="/site.webmanifest"/>
       </Head>
       <Grid
         templateAreas={`"nav header header" "nav main event" "footer footer footer"`}
